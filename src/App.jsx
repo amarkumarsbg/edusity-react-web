0ccbb1afd7bbd215fb4a199de6d47f82d182e1ac
amarkumarsbg <amarkumarsbg13@gmail.com>
2024-02-24 19:33:56 +0530

    complete responsiveness feature and video play feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import About from "./components/About/About";
 import Campus from "./components/Campus/Campus";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Programs from "./components/Programs/Programs";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Title from "./components/Title/Title";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 
 const App = () => {
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar />
@@ -15,14 +20,16 @@ const App = () => {
       <div className="container">
         <Title subTitle="Our PROGRAM" title="What we Offer" />
         <Programs />
-        <About />
+        <About setPlayState={setPlayState} />
         <Title subTitle="Gallery" title="Campus Photos" />
         <Campus />
         <Title subTitle="TESTIMONIALS" title="What's Student Says" />
         <Testimonials />
         <Title subTitle="Contact Us" title="Get in Touch" />
         <Contact />
+        <Footer />
       </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
     </div>
   );
 };
