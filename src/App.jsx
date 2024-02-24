5206c69838ab5b7f5906b7f8d36efb53953249ad
amarkumarsbg <amarkumarsbg13@gmail.com>
2024-02-24 16:56:35 +0530

    testimonials section completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Campus from "./components/Campus/Campus";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Programs from "./components/Programs/Programs";
+import Testimonials from "./components/Testimonials/Testimonials";
 import Title from "./components/Title/Title";
 
 const App = () => {
@@ -16,6 +17,8 @@ const App = () => {
         <About />
         <Title subTitle="Gallery" title="Campus Photos" />
         <Campus />
+        <Title subTitle="TESTIMONIALS" title="What's Student Says" />
+        <Testimonials />
       </div>
     </div>
   );
